55da56dd18ca74573e9df7333f8b7880ba93036f
Karl Fredrik <karl.fredrik.haugland@gmail.com>
2016-01-26 21:33:24 +0100

    The whole module, as of now.
    Todo:
        Write docs
        Implement rest of features

---
+++ core.py
@@ -0,0 +1,446 @@
+import re
+import socket
+
+
+# Reference table for the ETH8020 (for which this module was originally developed for)
+# (source: http://www.robot-electronics.co.uk/htm/eth8020tech.htm)
+# +-----+-----+----------------------------------------------------------------+---------------------------+---------+
+# |  Command  |                                 Action                         |          Return           | Implem- |
+# | dec | hex |                                                                |                           |  ented  |
+# +-----+-----+----------------------------------------------------------------+---------------------------+---------+
+# | 16  | 10  | Get Module Info - returns 3 bytes. Module Id (21 for ETH8020), | Module ID (1 byte),       |         |
+# |     |     | Hardware version, Firmware version.                            | HW version (1 byte)       |    x    |
+# |     |     |                                                                | FW version (1 byte)       |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 32  | 20  | Digital Active - follow with 1-20 to set relay on, then a time |                           |         |
+# |     |     | for pulsed output from 1-255 (100ms resolution) or 0 for       | 1 bit: 1/0 = success/fail |    x    |
+# |     |     | permanent                                                      |                           |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 33  | 21  | Digital Inactive - follow with 1-20 to turn relay off, then a  | 1 bit: 1/0 = success/fail |         |
+# |     |     | time for pulsed output from 1-255 (100ms resolution) or 0 for  |                           |    x    |
+# |     |     | permanent                                                      |                           |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 35  | 23  | Digital Set Outputs - follow with 3 bytes, first byte will set |                           |         |
+# |     |     | relays 1-8, All on = 255 (0xFF), All off = 0, 2nd byte for     | 1 bit: 1/0 = success/fail |         |
+# |     |     | relays 9-16, 3rd byte for relays 17-20                         |                           |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 36  | 24  | Digital Get Outputs - returns 3 bytes, the first corresponds   | 3 bytes:                  |         |
+# |     |     | with relays 1-8, 2nd byte for relays 9-16, 3rd byte for        |     1st: relay 1-8        |         |
+# |     |     | relays 17-20                                                   |     2nd; relay 9-16       |         |
+# |     |     |                                                                |     3rd: relay 17-20      |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 37  | 25  | Digital Get Inputs - returns 4 bytes, the first three bytes    | 4 bytes:                  |         |
+# |     |     | are always 0, the 4th bytes bits correspond with the 8 digital |     1st-3rd: 0            |         |
+# |     |     | inputs, a high bit meaning input is active (driven low)        |     4th: Digital IO 1-8   |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 50  | 32  | Get Analogue Voltage - follow with 1-8 for channel and ETH8020 |                           |         |
+# |     |     | will respond with 2 bytes to form an 16-bit integer (high byte |                           |         |
+# |     |     | first)                                                         |                           |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 58  | 3A  | ASCII text commands (V4+) - allows a text string to switch     |                           |         |
+# |     |     | outputs, see section below                                     |                           |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 119 | 77  | Get Serial Number - Returns the unique 6 byte MAC address of   |                           |         |
+# |     |     | the module.                                                    |                           |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 120 | 78  | Get Volts - returns relay supply voltage as byte, 125 being    |                           |         |
+# |     |     | 12.5V DC                                                       |                           |         |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 121 | 79  | Password Entry - see TCP/IP password                           |                           |    x    |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 122 | 7A  | Get Unlock Time - see section below                            |                           |    x    |
+# +-----+-----+----------------------------------------------------------------+---------------------------+         |
+# | 123 | 7B  | Log Out - immediately re-enables TCP/IP password protection    |                           |    x    |
+# +-----+-----+----------------------------------------------------------------+---------------------------+---------+
+
+# Will turn off print messages
+DEBUG = False
+
+COMMANDS = {
+        'get_module_info':  '\x10',
+        'set_relay_on':     '\x20',
+        'set_relay_off':    '\x21',
+        'set_relay_state':  '\x23',
+        'get_relay_state':  '\x24',
+        'get_analog_value': '\x32',
+        'ascii_command':    '\x3a',
+        'get_mac_address':  '\x77',
+        'get_volts':        '\x78',
+        'send_password':    '\x79',
+        'get_unlock_time':  '\x7a',
+        'log_out':          '\x7b',
+    }
+
+MODELS = {
+        18: {'name': 'ETH002',
+             'relays': 2,
+             'digital_io': 0,
+             'analog_input': 0
+            },
+
+        19: {'name': 'ETH008',
+             'relays': 8,
+             'digital_io': 0,
+             'analog_input': 0
+            },
+
+        20: {'name': 'ETH484',
+             'relays': 4,
+             'digital_io': 8,
+             'analog_input': 4
+            },
+
+        21: {'name': 'ETH8020', 
+             'relays': 20,
+             'digital_io': 0,
+             'analog_input': 8
+            },
+
+        29: {'name': 'ETH044', 
+             'relays': 4,
+             'digital_io': 4,
+             'analog_input': 0
+            }
+        }
+
+
+def string_only_contains_bits(string):
+    """
+    Check if a string only contains bits (0s and 1s)
+    """
+    search = re.compile(r'^[01]*$').search
+    return bool(search(string))
+
+
+def hex_to_int(data):
+    return int(data.encode('hex'), 16)
+
+
+def int_to_hex(data):
+    return chr(data)
+
+
+def bitstring_to_hex(bitstring):
+    hex_string = ''
+
+    # Typos to avoid using reserved namespace \o/
+    btyes = [bitstring[x:x+8] for x in range(0, len(bitstring), 8)]
+    for btye in btyes:
+        hex_string += chr(int(btye[::-1], 2))
+
+    return hex_string
+
+
+def hex_to_bitstring(hex_string):
+    bitstring = ''
+    
+    for btye in hex_string:
+        bitstring += bin(hex_to_int(btye))[2:].zfill(8)[::-1]
+
+    return bitstring
+
+
+class ETHRelay:
+    def __init__(self, ip, port=17494, password=None):
+        self.ip = ip
+        self.port = port
+        self.password = password
+
+        self.connect(ip, port, password)
+        self.get_module_info()
+        if self.no_relays > 0:
+            self.states = self.get_multiple_relays_state()
+        else:
+            self.states = {}
+        
+        
+    def connect(self, ip, port, password=None):
+        """
+        Try to connect to a module using the inputted parameters.
+
+        Return values:
+            True/False: Wether the connection was successfull or not
+        """
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connected = False
+        # Let's try to connect
+        try:
+            self.sock.connect((ip, port))
+        except Exception, e:
+            # The connection could fail for a multitide of reasons, in which
+            # we will propagate any exception thrown by socket up the stack
+            # by printing the exception
+            print e
+            self.sock.close()
+            return False
+            
+        # At this point we have a socket connection, and we must authenticate
+        # against the module if it requires so
+        if not self.get_unlock_time:
+            # If this is false, we need to authenticate with password
+            unlocked = self.unlock(password)
+            if not unlocked:
+                if DEBUG:
+                    print "Failed to unlock module. Try again."
+                return False
+            
+            self.connected = True 
+
+        return True
+
+    
+    def disconnect(self):
+        try:
+            self.lock()
+            self.sock.close()
+        except Exception, e:
+            print e
+        if DEBUG:
+            print "Disconnected"
+        return True
+
+        
+    def bitstring_to_dict(self, bitstring):
+        """
+        Turn a bitstring (like '10010111') into a dict like {1: True, 2: False ...}
+        for easier processing of the bit values based on their position
+        """
+    
+        vals = {}
+        for i, val in enumerate(bitstring):
+            # Make sure we don't think we have more relays than we actually have
+            if i >= self.no_relays:
+                break
+            vals[i+1] = bool(int(val))
+        
+        return vals
+    
+
+    def dict_to_bitstring(self, _dict):
+        """
+        Turn a dict (with integers as keys) into a bitstring
+        """
+        # Dirtily validate the keys
+        for k in _dict.keys():
+            if not isinstance(k, int):
+                print "Value %s is not integer (it is %s)." % (k, type(k))
+                return False
+            elif k > self.no_relays:
+                print "Value %s too large; number of relays on this module is %s." % (k, self.no_relays)
+        
+        vals = ''
+        # We want to iterate through the three bytes of data we might end up sending
+        for i in range(1, 25):
+            vals += str(int(_dict.get(i, False)))
+
+        return vals
+
+
+    def send_command(self, command, value=None, number_of_bytes=1):
+        """
+        number_of_bytes
+            This is the number of bytes we should expect back from the read function.
+            Most cases, we will expect a 1-byte response (default) but it can also be
+            easily overridden to expect a different response length.
+        """
+        if value:
+            command = command + value
+        try:
+            self.sock.sendall(command)
+        except Exception, e:
+            if DEBUG:
+                print "Error sending message"
+
+        return self.read_command_result(number_of_bytes)
+
+
+    def read_command_result(self, number_of_bytes):
+        """
+        Read the number of bytes from the socket
+        """
+        chunks = []
+        number_of_bytes_received = 0
+        while number_of_bytes_received < number_of_bytes:
+            if DEBUG:
+                print "Getting byte %d" % (number_of_bytes_received+1)
+            chunk = self.sock.recv(min(number_of_bytes - number_of_bytes_received, 2048))
+            if chunk == '':
+                print 'Error reading message - premature end of message'
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            number_of_bytes_received += len(chunk)
+        if DEBUG:
+            print 'Chunks:', [x.encode('hex') for x in chunks]
+        return ''.join(chunks)
+
+
+    def get_module_info(self):
+        """
+        Get info about our module and store it as attributes of self
+        """
+        result = self.send_command(COMMANDS['get_module_info'], number_of_bytes=3)
+        self.model_id = hex_to_int(result[0])
+        self.software_version = hex_to_int(result[1])
+        self.firmware_version = hex_to_int(result[2])
+
+        try:
+            model = MODELS[self.model_id]
+        except AttributeError:
+            if DEBUG:
+                print "Invalid model: %s is not defined in MODELS." % self.model_id
+            return False
+
+        self.no_relays = model['relays']
+        self.no_digital_io = model['digital_io']
+        self.no_analog_input = model['analog_input']
+        self.model_name = model['name']
+
+        return True
+
+
+    def get_unlock_time(self):
+        """
+        If locked and require password:
+            Returns False
+        If unlocked and will not become locked:
+            Returns True
+        If unlocked and will lock in a number of seconds
+            Returns the time before the module will lock
+        """
+        result = send_command(COMMANDS['get_unlock_time'])
+        if result[0] == 0:
+            # The module is locked and needs to be unlocked
+            return False
+        elif result[0] == 255:
+            # The module does not require password
+            return True
+        else:
+           # The module tends to require password, but is unlocked
+            return result[0]
+
+
+    def unlock(self, password):
+        """
+        Send a password to the module to unlock it
+        """
+        result = send_command(COMMANDS['send_password'], value=password)
+        
+        if result[0] == 1:
+            if DEBUG:
+                print "Wrong password"
+            self.sock.close()
+            return False
+        else:
+            return True
+
+
+    def lock(self):
+        """
+        Send a command to log out from the module
+        """
+        result = send_command(COMMANDS['log_out'])
+        success = result[0]
+        if success:
+            return True
+        else:
+            return False
+
+
+    def set_relay_on(self, relay, pulse=0):
+        """
+        Set the state of a single relay. This function can also trigger
+        pulse the relay. The pulse value is defined in the range of 1-255,
+        and translates to multiples of 100ms (100ms to 25.5s)
+        """
+        if pulse < 0 or pulse > 255:
+            return False
+
+        # The set_replay_on-command requires the number of the relay and an
+        # optional pulse value in the range of 0 and 255
+        values = relay + pulse
+        
+        # And this is a three-byte command (command + relay + pulse), so we
+        # need to specify that to send_command
+        result = self.send_command(COMMANDS['set_relay_on'], values, 3)
+        
+        if result[0]:
+            return True
+        else:
+            return False
+        
+
+    def set_relay_off(self, relay, pulse=0):
+        """
+        Set the state of a single relay. This function can also trigger
+        pulse the relay. The pulse value is defined in the range of 1-255,
+        and translates to multiples of 100ms (100ms to 25.5s)
+        """
+        if pulse < 0 or pulse > 255:
+            return False
+
+        # The set_replay_on-command requires the number of the relay and an
+        # optional pulse value in the range of 0 and 255
+        values = relay + pulse
+        result = self.send_command(COMMANDS['set_relay_off'], values)
+        
+        if result[0]:
+            return True
+        else:
+            return False
+
+
+    def set_multiple_relays_state(self, _dict):
+        """
+        Take a dict of relay states, turn it into a bitstring and send it off
+        to the relay to turn stuff on or off.
+        """
+        bitstring = self.dict_to_bitstring(_dict)
+        hex_string = bitstring_to_hex(bitstring)
+        result = self.send_command(COMMANDS['set_relay_state'], hex_string)
+        if DEBUG:
+            print "Setting values, bitstring %s" % bitstring
+
+        if result[0]:
+            return True
+        else:
+            return False
+
+
+    def set_relay_state(self, relay, state, turn_off_rest=False):
+        """
+        turn_off_rest:
+            Set all the other relays to open. The module usually does
+            this by default
+        """
+        
+        if turn_off_rest:
+            result = self.set_multiple_relays_state({relay: state})
+        else:
+            states = self.states
+            states[relay] = state
+            result = self.set_multiple_relays_state(states)
+            self.states = states
+
+        return result
+
+
+    def get_multiple_relays_state(self):
+        """
+        Ask the module to tell us about the state of all the relays
+        """
+        result = self.send_command(COMMANDS['get_relay_state'], number_of_bytes=3)
+        bitstring = hex_to_bitstring(result)
+        
+        if result:
+            return self.bitstring_to_dict(bitstring)
+        else:
+            return False
+
+    
+    def get_relay_state(self, relay):
+        """
+        Ask the module to tell us about the state of all the relays and find one of the relys
+        """
+        bitstring = self.get_multiple_relays_state()
+        return bool(int(bitstring[relay]))
+        
